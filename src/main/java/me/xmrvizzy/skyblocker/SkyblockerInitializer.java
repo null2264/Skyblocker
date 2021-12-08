0c766752608efcb9834bb2e724227944d5fe84cb
ziro <palembani@gmail.com>
2021-12-08 19:35:03 +0700

    + Upgrading to 1.18

--- src/main/java/me/xmrvizzy/skyblocker/SkyblockerInitializer.java
+++ src/main/java/me/xmrvizzy/skyblocker/SkyblockerInitializer.java
@@ -3,7 +3,13 @@ package me.xmrvizzy.skyblocker;
 import me.xmrvizzy.skyblocker.config.SkyblockerConfig;
 import me.xmrvizzy.skyblocker.skyblock.HotbarSlotLock;
 import me.xmrvizzy.skyblocker.skyblock.item.PriceInfoTooltip;
+import me.xmrvizzy.skyblocker.utils.ItemUtils;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.text.Text;
 
 public class SkyblockerInitializer implements ClientModInitializer
 {
@@ -12,5 +18,14 @@ public class SkyblockerInitializer implements ClientModInitializer
         HotbarSlotLock.init();
         SkyblockerConfig.init();
         PriceInfoTooltip.init();
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal("cooldown").executes(ctx -> {
+                FabricClientCommandSource source = ctx.getSource();
+                ClientPlayerEntity player = source.getPlayer();
+                player.sendMessage(Text.of(Integer.toString(ItemUtils.getLoreCooldown(player.getMainHandStack()))), false);
+                return 0;
+            }));
+        });
     }
 }
